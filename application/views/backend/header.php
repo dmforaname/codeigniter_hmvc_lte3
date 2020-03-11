c8714fa4cac32afdb1ea3fca657c48ffde384776
diazmahendra <widiasa@narrada.com>
2020-03-11 17:35:22 +0700

    update

--- application/views/backend/header.php
+++ application/views/backend/header.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-  <title>Kreazy.id | Hospital Information System</title>
+  <title>Kreazy.id</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
